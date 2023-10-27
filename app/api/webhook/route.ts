47bda42fad13c92ef5cb6bcc49da3664da5b3694
Rafael Alexander <rafael.tena@heru-app.com>
2023-10-26 22:02:41 -0600

    change

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,7 +1,17 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-    const data = await request.json();
+  const data = await request.json();
 
-  return NextResponse.json(data);
+  const response = await fetch(
+    "https://api.hubapi.com/conversations/v3/conversations/threads/" + JSON.stringify(data) + "/messages",
+    {
+      headers: {
+        Authorization: "Bearer pat-na1-ede60426-372b-4a88-a642-7835df95d896",
+      },
+    }
+  );
+
+  console.log(data, response);
+  return NextResponse.json(response.json());
 }
