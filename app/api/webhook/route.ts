64f26c0feddc4e9ae9beca79e430bbd46ba58056
Rafael Alexander <rafael.tena@heru-app.com>
2023-10-28 17:01:55 -0600

    hubspot

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -14,5 +14,10 @@ export async function POST(request: Request) {
   //   }
   // );
 
-  return NextResponse.json({ message: "Perfecto" });
+  return NextResponse.json({
+    botMessage:
+      "En Heru, nos dedicamos a simplificar y automatizar los procesos fiscales para miles de mexicanos. Ofrecemos una amplia gama de servicios, que incluyen asistencia con procedimientos del SAT, generación de certificados de situación fiscal, ayuda con declaraciones de impuestos mensuales y anuales. También apoyamos con la actualización de declaraciones mensuales pendientes y proporcionamos una forma fácil de ver y emitir facturas. Nuestros servicios atienden a una variedad de regímenes fiscales, lo que nos hace versátiles y accesibles para una amplia gama de clientes. Estamos disponibles en las plataformas Android e iOS, lo que nos hace fácilmente accesibles. ¿Hay algo específico que te gustaría saber sobre nuestros servicios?",
+    nextModuleNickname: "",
+    responseExpected: true,
+  });
 }
