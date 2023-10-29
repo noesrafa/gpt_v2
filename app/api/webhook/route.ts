49abb9140b09eb46fa13241f33071706c5d71e38
Rafael Alexander <rafael.tena@heru-app.com>
2023-10-28 18:33:37 -0600

    agent

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,25 +2,50 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const data = await request.json();
-  const response = await fetch(
-    "https://gpt-v2-git-main-noesrafa.vercel.app/api/openai",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        messages: [
-          {
-            role: "user",
-            content: data.userMessage.message,
-          },
-        ],
-      }),
+
+  const isDevelopment = process.env.NODE_ENV;
+  const API_URL = isDevelopment
+    ? "http://localhost:3000"
+    : "https://gpt-v2-git-main-noesrafa.vercel.app";
+
+    if (false) {
+      return NextResponse.json({
+        "hola": API_URL + "/api/openai",
+        "data": data.userMessage.message,
+      });
     }
-  );
+
+  const response = await fetch(API_URL + "/api/openai-webhook", {
+    method: "POST",
+    body: JSON.stringify({
+      messages: [
+        {
+          role: "user",
+          content: data.userMessage.message,
+        },
+      ],
+    }),
+  });
 
   const responseJson = await response.json();
+  console.log(responseJson);
+
+  if (responseJson?.choices?.[0]?.finish_reason === "function_call") {
+    const functionName = responseJson.choices[0]?.message?.function_call?.name;
+    console.log(functionName);
+    if (functionName === "talk_to_human") {
+      return NextResponse.json({
+        botMessage: "Claro, en un momento te atenderá un agente.",
+        nextModuleNickname: "",
+        responseExpected: false,
+      });
+    }
+  }
+  
+
   return NextResponse.json({
-    botMessage: responseJson.choices[0].message.content,
+    botMessage: responseJson.choices?.[0].message.content,
     nextModuleNickname: "",
-    responseExpected: false,
+    responseExpected: true,
   });
 }
