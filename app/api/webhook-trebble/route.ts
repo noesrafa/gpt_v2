5d9c4dd6c27be0faa8ab3432d9e1b565fb486ea0
Rafael Alexander <rafael.tena@heru-app.com>
2023-11-07 15:53:24 -0600

    new

--- app/api/webhook-trebble/route.ts
+++ app/api/webhook-trebble/route.ts
@@ -2,55 +2,48 @@ import { TrebleRequest } from "@/types";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  let responseFinal = {};
-  const data: TrebleRequest = await request.json();
+  const req: TrebleRequest = await request.json();
 
   const payload = {
     user_session_keys: [
       {
         key: "message",
-        value: data.session_id ?? "Sin session ID",
-      },
-      {
-        key: "request",
-        value: JSON.stringify(data) ?? "body vacio",
-      },
-      {
-        key: "url",
-        value:
-          `https://main.treble.ai/session/${data.session_id}/update` ??
-          "URL vacia",
-      },
-      {
-        key: "handoff",
-        value: false,
+        value: "Hello from webhook",
       },
     ],
   };
 
-  try {
-    console.log("PAYLOAD", payload);
-
-    await fetch(`https://main.treble.ai/session/${data.session_id}/update`, {
+  const response = await fetch(
+    `https://main.treble.ai/session/${req.session_id}/update`,
+    {
       method: "POST",
       body: JSON.stringify(payload),
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${"ak_uO0qsSyY94F0dKCDvD2-r0RRgCz8s5MCrA"}`,
       },
-    })
-      .then((res) => res.json())
-      .then((responseJson) => {
-        console.log("RESPONSE", responseJson);
-        responseFinal = responseJson;
-      })
-      .catch((error) => {
-        console.log("ERROR", error);
-        return NextResponse.json(error);
-      });
-  } catch (error) {
-    return NextResponse.json(error);
+    }
+  );
+  const resJson = await response.json();
+
+  console.log(
+    "\n\n\nPAYLOAD",
+    payload,
+    "\n\n\nURL",
+    `https://main.treble.ai/session/${req.session_id}/update`,
+    "\n\n\nREQ",
+    req,
+    "\n\n\nRES",
+    resJson
+  );
+
+  if (response.status !== 200) {
+    return NextResponse.json(
+      {
+        error: resJson.message,
+      },
+      { status: 500 }
+    );
   }
 
-  return NextResponse.json(responseFinal);
+  return NextResponse.json(resJson);
 }
