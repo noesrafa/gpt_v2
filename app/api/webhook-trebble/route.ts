5e103c216d8478177f8029caf2b0f2ec26666568
Rafael Alexander <rafael.tena@heru-app.com>
2023-11-07 14:29:29 -0600

    change

--- app/api/webhook-trebble/route.ts
+++ app/api/webhook-trebble/route.ts
@@ -2,90 +2,33 @@ import { TrebleRequest } from "@/types";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
+  let responseFinal = {};
   const data: TrebleRequest = await request.json();
 
-  const isDevelopment = process.env.NODE_ENV;
-  const API_URL = isDevelopment
-    ? "http://localhost:3000"
-    : "https://gpt-v2-git-main-noesrafa.vercel.app";
-
-  if (true) {
-    console.log("REQ", data);
-
-    const test = {
-      user_session_keys: [
-        {
-          key: "message",
-          value: "pamplinas",
-        },
-        {
-          key: "session_id",
-          value: data.session_id ?? "Vacio Session ID",
-        },
-      ],
-    };
-
-    const response = await fetch(
-      `https://main.treble.ai/session/${data.session_id}/update`,
-      {
-        method: "POST",
-        body: JSON.stringify(test),
-      }
-    );
-
-    await fetch(
-      "https://api.hubapi.com/conversations/v3/conversations/threads/" +
-        JSON.stringify({
-          version: "1.0",
-          url: `https://main.treble.ai/session/${data.session_id}/update`,
-          payload: test,
-          response: response ?? "Vacio",
-          session: data.session_id ?? "Vacio Session ID",
-          data,
-        }),
-      {
-        headers: {
-          Authorization: "Bearer pat-na1-ede60426-372b-4a88-a642-7835df95d896",
-        },
-      }
-    );
-
-    return NextResponse.json(test);
-  }
-
-  const response = await fetch(
-    "https://gpt-v2-git-main-noesrafa.vercel.app/api/openai-webhook",
-    {
+  try {
+    await fetch(`https://main.treble.ai/session/${data.session_id}/update`, {
       method: "POST",
       body: JSON.stringify({
-        messages: [
+        user_session_keys: [
           {
-            role: "user",
-            content: "hola",
+            key: "message",
+            value: data.session_id ?? "Vacio Session ID",
           },
         ],
       }),
-    }
-  );
-
-  const responseJson = await response.json();
-  console.log(responseJson);
-
-  if (responseJson?.choices?.[0]?.finish_reason === "function_call") {
-    const functionName = responseJson.choices[0]?.message?.function_call?.name;
-    console.log(functionName);
-    if (functionName === "talk_to_human") {
-      return NextResponse.json({
-        botMessage: null,
-        nextModuleNickname: "",
-        responseExpected: false,
+    })
+      .then((res) => res.json())
+      .then((responseJson) => {
+        console.log("RESPONSE", responseJson);
+        responseFinal = responseJson;
+      })
+      .catch((error) => {
+        console.log("ERROR", error);
+        return NextResponse.json(error);
       });
-    }
+  } catch (error) {
+    return NextResponse.json(error);
   }
 
-  return NextResponse.json({
-    botMessage: responseJson.choices?.[0].message.content,
-    nextModuleNickname: "",
-    responseExpected: true,
-  });
+  return NextResponse.json(responseFinal);
 }
