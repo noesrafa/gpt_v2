e28c05efe96cecade9b40236cbacdcd0d711e492
Rafael Alexander <rafael.tena@heru-app.com>
2023-11-07 14:57:51 -0600

    chagne

--- app/api/webhook-trebble/route.ts
+++ app/api/webhook-trebble/route.ts
@@ -5,27 +5,31 @@ export async function POST(request: Request) {
   let responseFinal = {};
   const data: TrebleRequest = await request.json();
 
+  const payload = {
+    user_session_keys: [
+      {
+        key: "message",
+        value: data.session_id ?? "Sin session ID",
+      },
+      {
+        key: "request",
+        value: JSON.stringify(data) ?? "body vacio",
+      },
+      {
+        key: "url",
+        value:
+          `https://main.treble.ai/session/${data.session_id}/update` ??
+          "URL vacia",
+      },
+    ],
+  };
+
   try {
+    console.log("PAYLOAD", payload);
+    
     await fetch(`https://main.treble.ai/session/${data.session_id}/update`, {
       method: "POST",
-      body: JSON.stringify({
-        user_session_keys: [
-          {
-            key: "message",
-            value: data.session_id ?? "Sin session ID",
-          },
-          {
-            key: "request",
-            value: JSON.stringify(data) ?? "body vacio",
-          },
-          {
-            key: "url",
-            value:
-              `https://main.treble.ai/session/${data.session_id}/update` ??
-              "URL vacia",
-          },
-        ],
-      }),
+      body: JSON.stringify(payload),
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${"ak_uO0qsSyY94F0dKCDvD2-r0RRgCz8s5MCrA"}`,
