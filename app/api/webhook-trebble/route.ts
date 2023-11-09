3a984369513a68bfe1a861b3898c235d0a3b6560
Rafael Alexander <rafael.tena@heru-app.com>
2023-11-09 15:04:04 -0600

    openai

--- app/api/webhook-trebble/route.ts
+++ app/api/webhook-trebble/route.ts
@@ -21,15 +21,15 @@ export async function POST(request: Request) {
 
   const responseOpenAIJson = await responseOpenAI.json();
 
-  function fakePromise() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve("La operación se completó exitosamente después de 20 segundos.");
-      }, 60000); // 20 segundos
-    });
-  }
+  // function fakePromise() {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve("La operación se completó exitosamente después de 20 segundos.");
+  //     }, 0000); // 20 segundos
+  //   });
+  // }
 
-  await fakePromise();
+  // await fakePromise();
 
   const payload = {
     user_session_keys: [
