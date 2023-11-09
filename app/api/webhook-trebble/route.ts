5dd8f3d814edba3eb2aac6e4e71c718ed500354c
Rafael Alexander <rafael.tena@heru-app.com>
2023-11-09 14:11:17 -0600

    openai

--- app/api/webhook-trebble/route.ts
+++ app/api/webhook-trebble/route.ts
@@ -4,60 +4,53 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const req: TrebleRequest = await request.json();
 
-  let resJson;
+  const responseOpenAI = await fetch(
+    "https://gpt-v2-git-main-noesrafa.vercel.app/api/openai-webhook",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        messages: [
+          {
+            role: "user",
+            content: req.actual_response,
+          },
+        ],
+      }),
+    }
+  );
+
+  const responseOpenAIJson = await responseOpenAI.json();
 
   const payload = {
     user_session_keys: [
       {
         key: "message",
-        value: "Hello from webhook",
-      },
-      {
-        key: "handoff",
-        value: "false",
+        value: responseOpenAIJson.choices?.[0].message.content,
       },
     ],
   };
 
-  const postTreble = async () => {
-    const response = await fetch(
-      `https://main.treble.ai/session/${req.session_id}/update`,
-      {
-        method: "POST",
-        body: JSON.stringify(payload),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    resJson = await response.json();
-
-    if (response.status !== 200) {
-      return NextResponse.json(
-        {
-          error: resJson.message,
-        },
-        { status: 500 }
-      );
+  const response = await fetch(
+    `https://main.treble.ai/session/${req.session_id}/update`,
+    {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json",
+      },
     }
-  };
+  );
+  const resJson = await response.json();
 
-  setTimeout(() => {
-    postTreble();
-  }, 60000);
+  if (response.status !== 200) {
+    return NextResponse.json(
+      {
+        error: resJson.message,
+      },
+      { status: 500 }
+    );
+  }
 
   console.log("\n\n\nREQ", req, "\n\n\nPAYLOAD", payload, "\n\n\nRES", resJson);
-
-  return NextResponse.json({
-    user_session_keys: [
-      {
-        key: "message",
-        value: "Hello from webhook",
-      },
-      {
-        key: "handoff",
-        value: "false",
-      },
-    ],
-  });
+  return NextResponse.json([]);
 }
