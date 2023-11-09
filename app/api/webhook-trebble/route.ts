5ba08321bfd981f3a642b19d05d7e87ddad32dd1
Rafael Alexander <rafael.tena@heru-app.com>
2023-11-09 14:40:06 -0600

    openai

--- app/api/webhook-trebble/route.ts
+++ app/api/webhook-trebble/route.ts
@@ -4,10 +4,54 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const req: TrebleRequest = await request.json();
 
-  fetch("https://gpt-v2-git-main-noesrafa.vercel.app/api/webhook-handler", {
-    method: "POST",
-    body: JSON.stringify(req),
-  });
+  const responseOpenAI = await fetch(
+    "https://gpt-v2-git-main-noesrafa.vercel.app/api/openai-webhook",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        messages: [
+          {
+            role: "user",
+            content: req.actual_response,
+          },
+        ],
+      }),
+    }
+  );
 
-  return NextResponse.json(JSON.stringify(req));
+  const responseOpenAIJson = await responseOpenAI.json();
+
+  const payload = {
+    user_session_keys: [
+      {
+        key: "message",
+        value: responseOpenAIJson.choices?.[0].message.content,
+      },
+    ],
+  };
+
+  const response = await fetch(
+    `https://main.treble.ai/session/${req.session_id}/update`,
+    {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const resJson = await response.json();
+
+  if (response.status !== 200) {
+    return NextResponse.json(
+      {
+        error: resJson.message,
+      },
+      { status: 500 }
+    );
+  }
+
+  console.log("\n\n\nREQ", req, "\n\n\nPAYLOAD", payload, "\n\n\nRES", resJson);
+
+  return NextResponse.json([]);
 }
